e-user/nixpkgs

commit 16863d8ffe24649fb6a7500f2b457dd2e559de47

libxslt: fix impurity on 'native-stdenv' platforms
libxslt has optional dependencies which may be found in /usr or
/usr/local on platforms that have a native stdenv. With those features
enabled, the build generated binaries that depend on libraries outside
of the store. In this particular case, the NixOS channel had binaries
for FreeBSD that depended on libgcrypt, apparently because that packages
happens to be installed outside of Nix on the build machine. On other
machines, however, those binaries failed with unresolvable references.

pkgs/development/libraries/libxslt/default.nix
@@ -8,7 +8,14 @@ stdenv.mkDerivation rec {
     sha256 = "09ky3vhlaahvsb0q9gp6h3as53pfj70gincirachjqzj46jdka5n";
   };
 
-  buildInputs = [ libxml2 ];
+  configureFlags = [
+    "--with-libxml-prefix=${libxml2}"
+    "--without-python"
+    "--without-crypto"
+    "--without-debug"
+    "--without-mem-debug"
+    "--without-debugger"
+  ];
 
   postInstall = ''
     mkdir -p $out/nix-support

commit 379ec965bdd87145fe44f6dc9952172c692082a7

libxslt: revert impurity fix on 'native-stdenv' platforms
Commit 16863d8ffe24649fb6a7500f2b457dd2e559de47 caused massive re-builds.
This change needs to be made differently.

pkgs/development/libraries/libxslt/default.nix
@@ -8,14 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "09ky3vhlaahvsb0q9gp6h3as53pfj70gincirachjqzj46jdka5n";
   };
 
-  configureFlags = [
-    "--with-libxml-prefix=${libxml2}"
-    "--without-python"
-    "--without-crypto"
-    "--without-debug"
-    "--without-mem-debug"
-    "--without-debugger"
-  ];
+  buildInputs = [ libxml2 ];
 
   postInstall = ''
     mkdir -p $out/nix-support