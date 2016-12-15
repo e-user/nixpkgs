c4d8d0ea30c6603e689b1e00dddd313c6b65293e
Alexander Kahl <ak@sodosopa.io>
2016-12-15 16:16:18 +0100

    sssd: update to 1.14.2

--- pkgs/os-specific/linux/sssd/default.nix
+++ pkgs/os-specific/linux/sssd/default.nix
@@ -1,24 +1,26 @@
-{ stdenv, fetchurl, pkgs, augeas, bind, c-ares, cyrus_sasl, ding-libs, libnl,
-  libunistring, nss, samba, libnfsidmap, doxygen, python, python3,
-  pam, popt, talloc, tdb, tevent, pkgconfig, ldb, openldap, pcre, kerberos,
-  cifs_utils, glib, keyutils, dbus, fakeroot, libxslt, libxml2,
-  docbook_xml_xslt, ldap, systemd, nspr, check, cmocka,
-  uid_wrapper, nss_wrapper, docbook_xml_dtd_44, ncurses, Po4a }:
+{ stdenv, fetchurl, pkgs, lib, glibc, augeas, bind, c-ares,
+  cyrus_sasl, ding-libs, libnl, libunistring, nss, samba, libnfsidmap, doxygen,
+  python, python3, pam, popt, talloc, tdb, tevent, pkgconfig, ldb, openldap,
+  pcre, kerberos, cifs_utils, glib, keyutils, dbus, fakeroot, libxslt, libxml2,
+  docbook_xml_xslt, ldap, systemd, nspr, check, cmocka, uid_wrapper,
+  nss_wrapper, docbook_xml_dtd_44, ncurses, Po4a, http-parser, jansson }:
 
 let
   name = "sssd";
-  version = "1.13.3";
-in stdenv.mkDerivation {
+  version = "1.14.2";
+  docbookFiles = "${pkgs.docbook_xml_xslt}/share/xml/docbook-xsl/catalog.xml:${pkgs.docbook_xml_dtd_44}/xml/dtd/docbook/catalog.xml";
+in
+stdenv.mkDerivation {
   inherit name;
   inherit version;
 
   src = fetchurl {
     url = "https://fedorahosted.org/released/${name}/${name}-${version}.tar.gz";
-    sha256 = "3fd8fe8e6ee9f50b33eecd1bcccfaa44791f30d4e5f3113ba91457ba5f411f85";
+    sha1 = "167b2216c536035175ff041d0449e0a874c68601";
   };
 
   preConfigure = ''
-    export SGML_CATALOG_FILES="${pkgs.docbook_xml_xslt}/share/xml/docbook-xsl/catalog.xml:${pkgs.docbook_xml_dtd_44}/xml/dtd/docbook/catalog.xml"
+    export SGML_CATALOG_FILES="${docbookFiles}"
     export PYTHONPATH=${ldap}/lib/python2.7/site-packages
     export PATH=$PATH:${pkgs.openldap}/libexec
     export CPATH=${pkgs.libxml2.dev}/include/libxml2
@@ -37,6 +39,7 @@ in stdenv.mkDerivation {
       --without-semanage
       --with-xml-catalog-path=''${SGML_CATALOG_FILES%%:*}
       --with-ldb-lib-dir=$out/modules/ldb
+      --with-nscd=${glibc.bin}/sbin/nscd
     )
   '';
 
@@ -46,15 +49,27 @@ in stdenv.mkDerivation {
                   talloc tdb tevent pkgconfig ldb pam openldap pcre kerberos
                   cifs_utils glib keyutils dbus fakeroot libxslt libxml2
                   ldap systemd nspr check cmocka uid_wrapper
-                  nss_wrapper ncurses Po4a ];
+                  nss_wrapper ncurses Po4a http-parser jansson ];
 
-  buildPhase = ''
-    substituteInPlace config.h --replace "<HAVE_KRB5_SET_TRACE_CALLBACK>" ""
-    make SGML_CATALOG_FILES="$SGML_CATALOG_FILES"
-  '';
+  makeFlags = [
+    "SGML_CATALOG_FILES=${docbookFiles}"
+  ];
 
   installPhase = ''
-    make SGML_CATALOG_FILES="$SGML_CATALOG_FILES" sysconfdir="$out/etc" localstatedir="$out/var" pidpath="$out/run" sss_statedir="$out/var/lib/sss" logpath="$out/var/log/sssd" pubconfpath="$out/var/lib/sss/pubconf" dbpath="$out/var/lib/sss/db" mcpath="$out/var/lib/sss/mc" pipepath="$out/var/lib/sss/pipes" gpocachepath="$out/var/lib/sss/gpo_cache" initdir="$out/rc.d/init.d" install
+    make install \
+     sysconfdir="$out/etc" \
+     localstatedir="$out/var" \
+     pidpath="$out/run" \
+     sss_statedir="$out/var/lib/sss" \
+     logpath="$out/var/log/sssd" \
+     pubconfpath="$out/var/lib/sss/pubconf" \
+     dbpath="$out/var/lib/sss/db" \
+     mcpath="$out/var/lib/sss/mc" \
+     pipepath="$out/var/lib/sss/pipes" \
+     gpocachepath="$out/var/lib/sss/gpo_cache" \
+     secdbpath="$out/var/lib/sss/secrets" \
+     initdir="$out/rc.d/init.d"
+
     rm -rf "$out"/run
     rm -rf "$out"/rc.d
     rm -f "$out"/modules/ldb/memberof.la
